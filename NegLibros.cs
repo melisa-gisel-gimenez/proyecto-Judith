718dcd245d1ce0741e1a67698233a5dba060ca19
melisa-gisel-gimenez <53663376+melisa-gisel-gimenez@users.noreply.github.com>
2022-11-09 01:20:24 -0300

    cargo la capa de negocio
    subo los archivos de la capa de negocio, no se porque en mi compu se guardo en una carpeta aparte del proyecto

---
+++ NegLibros.cs
@@ -0,0 +1,27 @@
+﻿using capa_Datos;
+using capa_Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CapaNegocio
+{
+    public class NegLibros
+    {
+        AdministrarTablas DatosObjLibros = new AdministrarTablas();
+        public int AbmLibros(string accion, Libros objlibros)
+        {
+            return DatosObjLibros.AbmLibros(accion, objlibros);
+        }
+
+        public DataSet listadoLibros(string cual)
+        {
+            return DatosObjLibros.listadoLibros(cual);
+        }
+
+    }
+}
---
+++ Negocio.cs
@@ -0,0 +1,24 @@
+﻿using capaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CapaNegocio
+{
+    public class Negocio
+    {
+        public bool probarConexion()
+        {
+            DatosConexion nuevaConexion;
+            nuevaConexion = new DatosConexion();
+
+            bool exitosa = nuevaConexion.Abrirconexion();
+            nuevaConexion.Cerrarconexion();
+
+            return exitosa;
+        }
+    }
+}
